38eb17dda66bdd593d9f029b6f4be31573f846ca
Hemant Tailor <hemant.tailor@live.co.uk>
2015-11-21 19:14:04 +0000

    Added properties and str method.

--- emailer.py
+++ emailer.py
@@ -9,45 +9,71 @@ from smtplib import SMTP
 
 class Email(object):
 
-    def __init__(self, subject, sender, recipients, message):
-        self.subject = subject
-        self.sender = sender
-        self.recipients = recipients
-        self.message = message
-        self.msg = None
+    def __init__(self, sender, recipients, subject, message):
+        self._sender = sender
+        self._recipients = recipients
+        self._subject = subject
+        self._message = message
+        self._msg = None
+
+    def __str__(self):
+        return ", ".join([sender, ", ".join(recipients), subject, message])
+
+    @property
+    def sender(self):
+        return self._sender
+
+    @property
+    def recipients(self):
+        return self._sender
+
+    @property
+    def subject(self):
+        return self._subject
+
+    @property
+    def message(self):
+        return self._message
 
     def send(self):
         self.__generate_email__()
-        self.msg["Subject"] = self.subject
-        self.msg["From"] = self.sender
-        self.msg["To"] = ",".join(self.recipients) if type(self.recipients) is list else self.recipients
+        self._msg["Subject"] = self._subject
+        self._msg["From"] = self._sender
+        self._msg["To"] = ",".join(self._recipients) if type(self._recipients) is list else self._recipients
         s = SMTP()
         s.connect()
-        s.sendmail(self.sender, self.recipients, self.msg.as_string())
+        s.sendmail(self._sender, self._recipients, self._msg.as_string())
 
     def __generate_email__(self):
-        self.msg = MIMEText(self.message)
+        self._msg = MIMEText(self._message)
 
 
 class EmailAttachment(Email):
 
-    def __init__(self, subject, sender, recipients, message, files):
-        super(EmailAttachment, self).__init__(subject, sender, recipients, message)
-        self.files = files
+    def __init__(self, sender, recipients, subject, message, files):
+        super(EmailAttachment, self).__init__(sender, recipients, subject, message)
+        self._files = files
         if type(self.files) is not list:
-            self.files = [self.files]
+            self._files = [self._files]
+
+    def __str__(self):
+        return super(EmailAttachment, self).__str__() + ", " + ", ".join(files)
+
+    @property
+    def files(self):
+        return self._files
 
     def compress(self):
         zfiles = []
-        for file in self.files:
+        for file in self._files:
             _zfile = file.split(".")[0] + ".zip"
             with zipfile.ZipFile(_zfile, "w", zipfile.ZIP_DEFLATED) as _zipped_file:
                 _zipped_file.write(file)
             zfiles.append(_zfile)
-        self.files = zfiles
+        self._files = zfiles
 
     def __attach_files__(self):
-        for file_name in self.files:
+        for file_name in self._files:
             if type(file_name) in (list, tuple):
                 file_name, output_filename = file_name
             else:
@@ -58,29 +84,33 @@ class EmailAttachment(Email):
             part.set_payload(open(file_name, "rb").read())
             Encoders.encode_base64(part)
             part.add_header("Content-Disposition", "attachment; filename=%s" % output_filename)
-            self.msg.attach(part)
+            self._msg.attach(part)
 
     def __generate_email__(self):
-        self.msg = MIMEMultipart()
-        self.msg.attach(MIMEText(self.message))
+        self._msg = MIMEMultipart()
+        self._msg.attach(MIMEText(self._message))
         self.__attach_files__()
 
 
 if __name__ == "__main__":
 
     subject = "Emailer Test"
-    sender = ""
-    recipients = ""
+    sender = "a@b.com"
+    recipients = "b@c.com", "c@d.com"
     message = "This is just a test message"
-    files = ""
+    files = "file1", "file2"
 
-    e = Email(subject, sender, recipients, message)
-    e.send()
+    e = Email(sender, recipients, subject, message)
+    print e
+    # e.send()
+
+    f = EmailAttachment(sender, recipients, subject, message, files)
+    print f
 
     # test sending an email with attachment
-    e = EmailAttachment(subject, sender, recipients, message, files)
-    e.send()
+    # e = EmailAttachment(subject, sender, recipients, message, files)
+    # e.send()
 
     # test sending an email with a compressed attachment
-    e.compress()
-    e.send()
+    # e.compress()
+    # e.send()
